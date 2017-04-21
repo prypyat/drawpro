e673c2ddc7cddf6f8694e53db57fd416bb94625a
20056142 <20056142@it102imac22.wit.ie>
2017-04-21 15:17:44 +0100

    Initial Commit

---
+++ drawproTests/drawproTests.swift
@@ -0,0 +1,36 @@
+//
+//  drawproTests.swift
+//  drawproTests
+//
+//  Created by 20056142 on 21/04/2017.
+//  Copyright © 2017 20056142. All rights reserved.
+//
+
+import XCTest
+@testable import drawpro
+
+class drawproTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
