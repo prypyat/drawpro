a8c137991d17429a78fd983026392731f8e0fe51
prypyat <kivcelik@Kivs-Mac.local>
2017-04-27 16:09:47 -0700

    clean2

--- drawpro/ViewController.swift
+++ drawpro/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 {
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var atleast: UILabel!
+    @IBOutlet weak var rText: UILabel!
+    @IBOutlet weak var lText: UILabel!
+    
     
     let deck2 = Array(1...60)
     
@@ -41,22 +45,46 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         return deck2.count
     }
     
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
-    {
-        calculate()
-    }
-    
-    
-    
-    @IBAction func calculate()
+    @IBAction func calculate(_ sender: UIButton)
     {
         let t = pickerView.selectedRow(inComponent: 3)+1
         let n = pickerView.selectedRow(inComponent: 2)+1
         let r = pickerView.selectedRow(inComponent: 1)+1
         let p = pickerView.selectedRow(inComponent: 0)+1
         
-        let test = Hypergeometric(numberOfTrials: n, requiredSuccesses: r, population: p)
-        result.text = String(format: "%.4f", test.probability(of: t)*100)+"%"
+        if (r > p)
+        {
+            let alert = UIAlertController(title: "Error",
+                                          message: "Number of target cards larger than total number of cards in deck",
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else if (n > p)
+        {
+            let alert = UIAlertController(title: "Error",
+                                          message: "Number of cards to draw larger than total number of cards in deck",
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else if (t > n)
+        {
+            let alert = UIAlertController(title: "Error",
+                                          message: "Number of target cards in draw larger than total number of cards in draw",
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else
+        {
+            let test = Hypergeometric(numberOfTrials: n, requiredSuccesses: r, population: p)
+            result.text = String(format: "%.02f", test.probability(of: t)*100)+"%"
+            atleast.text = String(format: "%.02f", 100-(test.distribution(.less(t))*100))+"%"
+            rText.text = "Chance of exactly \(t) draws"
+            lText.text = "Chance of at least \(t) draws"
+
+        }
     }
 }
 
