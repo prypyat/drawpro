663d3c9c2ee891ad20230a6085a2e9de5078bc38
prypyat <kivcelik@Kivs-Mac.local>
2017-04-27 15:18:52 -0700

    cleanv1

--- drawpro/ViewController.swift
+++ drawpro/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
+{
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var result: UILabel!
+    
+    let deck2 = Array(1...60)
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 4
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return String(deck2[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return deck2.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        calculate()
+    }
+    
+    
+    
+    @IBAction func calculate()
+    {
+        let t = pickerView.selectedRow(inComponent: 3)+1
+        let n = pickerView.selectedRow(inComponent: 2)+1
+        let r = pickerView.selectedRow(inComponent: 1)+1
+        let p = pickerView.selectedRow(inComponent: 0)+1
+        
+        let test = Hypergeometric(numberOfTrials: n, requiredSuccesses: r, population: p)
+        result.text = String(format: "%.4f", test.probability(of: t)*100)+"%"
+    }
 }
 
